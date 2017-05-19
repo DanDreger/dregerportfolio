1452a2716bcbb7d32c3a8389e8f8cb8a8e12ee74
Daniel Dreger <trigger125@hotmail.com>
2017-05-18 21:01:32 -0700

    links fixed, folders restructured, pages uploaded, JS file created

---
+++ js/main.js
@@ -0,0 +1,50 @@
+
+console.log("test")
+
+function horoscope(){
+               console.log("horoscope/index.html");
+               window.open("http://www.dandreger.com/horoscope/index.html")
+           }
+
+
+function bootstrap(){
+               console.log("colorfulflat/index.html");
+               window.open("http://www.dandreger.com/colorfulflat/index.html")
+           }
+
+function jenniferGrenz(){
+               console.log("jengrenz/index.html");
+               window.open("http://www.dandreger.com/jengrenz/index.html")
+           }
+
+function musicFinder(){
+               console.log("musicfinder/index.html");
+               window.open("http://www.dandreger.com/musicfinder/index.html")
+           }
+
+function eCommerce(){
+               console.log("ecommercepage/index.html");
+               window.open("http://www.dandreger.com/ecommercepage/index.html")
+           }
+
+
+function googleBooks(){
+               console.log("booksearch/index.html");
+               window.open("http://www.dandreger.com/booksearch/index.html")
+           }
+
+function alarmClock(){
+               console.log("clock/index.html");
+               window.open("http://www.dandreger.com/clock/index.html")
+           }
+
+function metro(){
+               console.log("Metro/index.html");
+               window.open("http://www.dandreger.com/Metro/index.html")
+           }
+
+function toDoList(){
+               console.log("http://codepen.io/qwawer/pen/vgEXLr");
+               window.open("http://codepen.io/qwawer/pen/vgEXLr")
+           }
+
